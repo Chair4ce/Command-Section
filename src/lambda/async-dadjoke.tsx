0d24831209866a006002b2889d59839f3f2a1e28
jacy hoag <jacylh@gmail.com>
2022-12-17 02:16:50 -0500

    switched app to typescript

--- src/lambda/async-dadjoke.tsx
+++ src/lambda/async-dadjoke.tsx
@@ -1,6 +1,7 @@
 // example of async handler using async-await
 // https://github.com/netlify/netlify-lambda/issues/43#issuecomment-444618311
 
+// @ts-ignore
 import axios from "axios"
 export async function handler(event, context) {
   try {
